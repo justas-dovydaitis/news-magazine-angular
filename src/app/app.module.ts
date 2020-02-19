bc97ac9b39d7fc156c03c0967661e7419b50d219
Justas <justas.dovydaitis@ktu.edu>
2020-02-19 15:18:37 +0200

    Added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { PostComponent } from './components/post/post.component';
+import { FeaturedPostComponent } from './components/featured-post/featured-post.component';
+import { LatestPostComponent } from './components/latest-post/latest-post.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { SelectComponent } from './components/inputs/select/select.component';
+import { TextareaComponent } from './components/inputs/textarea/textarea.component';
+import { FileComponent } from './components/inputs/file/file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    FeaturedPostComponent,
+    LatestPostComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    PostFormComponent,
+    SelectComponent,
+    TextareaComponent,
+    FileComponent
   ],
   imports: [
     BrowserModule,
